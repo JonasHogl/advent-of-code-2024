64bf6ff9f140e562ae5e47ef383415133e6ba114
JonasHogl <jonas_hoglund_@live.com>
2024-12-01 17:48:17 +0100

    mek

---
+++ lib/advent_of_code/day_01.ex
@@ -0,0 +1,8 @@
+defmodule AdventOfCode.Day01 do
+  def part1(args) do
+    args
+  end
+
+  def part2(_args) do
+  end
+end
---
+++ test/advent_of_code/day_01_test.exs
@@ -0,0 +1,22 @@
+defmodule AdventOfCode.Day01Test do
+  use ExUnit.Case
+
+  import AdventOfCode.Day01
+
+  test "part1" do
+    input = """
+    3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3\
+    """
+
+    expected_result = 11
+
+    result = part1(input)
+
+    assert result == expected_result
+  end
+end
